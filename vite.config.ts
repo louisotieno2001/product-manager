5f3662c6a21b59897d25f39d890c2243d1a873a3
louis2001 <louisotieno2001@gmail.com>
2025-11-25 14:10:28 +0300

    Fix: Corrected base path in vite.config.ts to use lowercase repository name for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Product-manager/',
+  base: '/product-manager/',
   plugins: [
     vue(),
     vueDevTools(),
