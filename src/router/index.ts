c977e65972fc78bc952a5f8d705abd342e25851f
louis2001 <louisotieno2001@gmail.com>
2025-11-25 14:30:53 +0300

    Buildable app

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LoginPage from '../pages/Login.vue';
 import ProductList from '../pages/Products.vue';
 import ViewProductPage from '../pages/ProductDetails.vue';
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 });
 
